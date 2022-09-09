4e95e44b237720774b82d67abb5790d79510f5cf
Krosh01 <muhambetovmels@gmail.com>
2022-09-09 16:45:10 +0600

    first commit

---
+++ svetofor.js
@@ -0,0 +1,12 @@
+let arg = prompt('Веддите Цвет');
+switch (arg){
+    case 'red':
+        alert('stop if you wonna stay alive')
+        break;
+    case 'yellow':
+        alert('wait a minate')
+        break;
+    case 'green':
+        alert('You can go niga')
+        break;
+}
